e3bcd54db0c498c8deeee0dc8f42b0cf99199b97
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2023-12-20 20:45:33 +0100

    Added colors, the function, file and line where the log was from.

--- src/logger.h
+++ src/logger.h
@@ -1,5 +1,9 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
 #include <source_location>
 #include <string>
+#include <unordered_map>
 
 class Logger {
   public:
@@ -9,3 +13,4 @@ class Logger {
     static void warn(std::string message, const std::source_location location = std::source_location::current());
     static void error(std::string message, const std::source_location location = std::source_location::current());
 };
+#endif
